aaf0a59fd140efa5edaec203a52c8280d4b51b29
BINTANG RIZQI PASHA <87906435+BintangRP@users.noreply.github.com>
2022-11-10 23:58:23 +0700

    commit progress #2: have bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./src/index.html'],
   theme: {
     extend: {},
     fontFamily: {
-      header:['DM Sans', 'sans-serif']
+      'header': ['DM Sans', 'sans-serif'],
+    },
+    backgroundImage: {
+      'foto1': "url('/assets/gambar/item.png')",
+      'foto2': "url('/assets/gambar/item1.png')",
+      'foto3': "url('/assets/gambar/item2.png')",
+      'foto4': "url('/assets/gambar/item3.png')",
     }
   },
   plugins: [],
